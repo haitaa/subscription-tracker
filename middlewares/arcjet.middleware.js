54949a4613da1cca0537efdf8c487d288e883e1a
haitaa <mustafahaitaa@gmail.com>
2025-05-02 16:08:09 +0300

    reminder workflow

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -1,19 +1,19 @@
-import aj from "../config/arcjet.js";
+import aj from '../config/arcjet.js';
 
 const arcjetMiddleware = async (req, res, next) => {
     try {
-        const decision = await aj.protect(req);
+        const decision = await aj.protect(req, { requested: 1 });
 
-        if (decision.isDenied()) {
-            if (decision.reason.isRateLimit()) return res.status(429).json({ success: false, error: "Too many requests." });
-            if (decision.reason.isBot()) return res.status(403).json({ success: false, error: "Bots are not allowed." });
+        if(decision.isDenied()) {
+            if(decision.reason.isRateLimit()) return res.status(429).json({ error: 'Rate limit exceeded' });
+            if(decision.reason.isBot()) return res.status(403).json({ error: 'Bot detected' });
 
-            return res.status(403).json({ success: false, error: "Access denied." });
+            return res.status(403).json({ error: 'Access denied' });
         }
 
         next();
     } catch (error) {
-        console.log(error);
+        console.log(`Arcjet Middleware Error: ${error}`);
         next(error);
     }
 }
